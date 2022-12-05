6013c580b443bf14ef3c9b74b79543f06b0518d2
Kanishk Kumar <kanishka_408@yahoo.co.in>
2022-12-05 11:20:16 +0000

    Initial Commit

---
+++ GraphQL-Country/GraphQL_CountryApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraphQL_CountryApp.swift
+//  GraphQL-Country
+//
+//  Created by Kanishk Kumar on 05/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct GraphQL_CountryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
